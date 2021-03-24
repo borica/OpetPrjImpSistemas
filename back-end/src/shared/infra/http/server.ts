e7a5d3eaab832197805f6596c738702d685eb7e9
joaouriel10 <joaouriel2016@outlook.com>
2021-03-23 21:25:27 -0300

    Finalizando back

--- back-end/src/shared/infra/http/server.ts
+++ back-end/src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -13,6 +14,7 @@ import AppError from '@shared/errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
