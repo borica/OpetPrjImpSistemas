93a97446e6ff361b41305500e65e71299c6b31a2
joaouriel10 <joaouriel2016@outlook.com>
2021-03-29 18:14:55 -0300

    Tela de resetar senha

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -6,14 +6,16 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Forgot from '../pages/Forgot';
-
+import RestPassword from '../pages/RestPassword';
 import Dashboard from '../pages/Dashboard';
+
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path='/' exact component={SignIn} />
         <Route path='/forgot' component={Forgot} />
         <Route path='/signup' component={SignUp} />
+        <Route path='/reset-password' component={RestPassword} />
 
         <Route path='/dashboard' component={Dashboard} isPrivate />
     </Switch>
